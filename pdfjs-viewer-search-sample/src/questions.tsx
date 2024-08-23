10c1f55e2038cf21c56c998c3e0515f9de5a9ee2
Lee Cattarin <lecattar@microsoft.com>
2024-08-23 11:09:38 -0700

    Remove textlayer; fetch page-aware canvases

--- pdfjs-viewer-search-sample/src/questions.tsx
+++ pdfjs-viewer-search-sample/src/questions.tsx
@@ -3,14 +3,29 @@ import di from "../../di.json"
 
 const response = di as docIntResponse;
 
-const preDraw = (iframeRef: React.RefObject<HTMLIFrameElement>, polygons: number[]) => {
-    const element = iframeRef.current?.contentWindow?.document.querySelector("div.canvasWrapper > canvas") as HTMLCanvasElement;
+const draw = (context: CanvasRenderingContext2D, scale: number = 1, polygons: number[]) => {
+    const multiplier = 72 * (window.devicePixelRatio || 1) * scale;
+    context.fillStyle = 'rgba(252, 207, 8, 0.3)';
+    context.strokeStyle = '#fccf08';
+    context.lineWidth = 1;
+    context.beginPath();
+    context.moveTo(polygons[0] * multiplier, polygons[1] * multiplier);
+    for (let i = 2; i < polygons.length; i += 2) {
+        context.lineTo(polygons[i] * multiplier, polygons[i + 1] * multiplier);
+    }
+    context.closePath();
+    context.fill();
+    context.stroke();
+};
+
+const preDraw = (iframeRef: React.RefObject<HTMLIFrameElement>, pageNumber: number, polygons: number[]) => {
+    const elements = iframeRef.current?.contentWindow?.document.querySelectorAll("div.canvasWrapper > canvas") as NodeList;
+    const element = elements[pageNumber - 1] as HTMLCanvasElement;
     if (element) {
         const highlightContext = element?.getContext('2d');
-        const textLayer = iframeRef.current?.contentWindow?.document.querySelector("div.textLayer") as HTMLDivElement;
-        const textLayerScale = parseFloat(iframeRef.current?.contentWindow?.getComputedStyle(textLayer).getPropertyValue('--scale-factor') || '1');
+        const scale = parseFloat(iframeRef.current?.contentWindow?.getComputedStyle(element).getPropertyValue('--scale-factor') || '1');
         if (highlightContext) {
-            draw(highlightContext, textLayerScale, polygons);
+            draw(highlightContext, scale, polygons);
         }
     }
 }
@@ -36,25 +51,10 @@ const findReferences = ({ text, fileName }: { text: string, fileName: string }, 
     // bounding box
     if (boundingRegions) {
         const { pageNumber, polygon } = boundingRegions[0];
-        preDraw(iframeRef, polygon);
         setFilePage(pageNumber);
+        preDraw(iframeRef, pageNumber, polygon);
     }
 }
-
-const draw = (context: CanvasRenderingContext2D, scale: number = 1, polygons: number[]) => {
-    const multiplier = 72 * (window.devicePixelRatio || 1) * scale;
-    context.fillStyle = 'rgba(252, 207, 8, 0.3)';
-    context.strokeStyle = '#fccf08';
-    context.lineWidth = 1;
-    context.beginPath();
-    context.moveTo(polygons[0] * multiplier, polygons[1] * multiplier);
-    for (let i = 2; i < polygons.length; i += 2) {
-        context.lineTo(polygons[i] * multiplier, polygons[i + 1] * multiplier);
-    }
-    context.closePath();
-    context.fill();
-    context.stroke();
-};
 
 const References = (props) => {
     const { references, setFilePage, iframeRef } = props;
@@ -63,7 +63,6 @@ const References = (props) => {
         const reference = references[index];
         returnArray.push(
             <div key={"reference" + index}>
-                <p id="referenceTitle">Reference contexts</p>
                 <p id="referenceContext">{reference.text}</p>
                 <button onClick={() => findReferences(reference, setFilePage, iframeRef)}>Display Document</button>
             </div>
@@ -77,6 +76,7 @@ export function QuestionAnswer(props) {
         <div id="question-container">
             <div><div id="question">Question: </div><div id="question-text">{qA.question}</div></div>
             <label id="answer" htmlFor="answer">Answer: </label><input name="answer" placeholder={qA.answer} />
+            <p id="referenceTitle">Reference contexts</p>
             <References references={qA.references} {...otherProps} />
         </div>
     )
