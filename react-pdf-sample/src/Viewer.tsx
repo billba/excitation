f44ea36d9a2883081e9540a87db59167e33f294b
Bill Barnes <billba@microsoft.com>
2024-09-18 15:51:38 -0700

    better lifecycle method for this

--- react-pdf-sample/src/Viewer.tsx
+++ react-pdf-sample/src/Viewer.tsx
@@ -16,37 +16,34 @@ export function Viewer() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
   const onDocumentLoadSuccess = useCallback(() => {
-    setTimeout(() => {
-      const canvas = document.getElementsByClassName('react-pdf__Page__canvas')[0] as HTMLCanvasElement;
-      console.log(canvas);
-      const styles = window.getComputedStyle(canvas);
-      const highlightCanvas = canvasRef.current!;
-      highlightCanvas.style.position = 'absolute';
-      highlightCanvas.style.top = styles.top;
-      highlightCanvas.style.left = styles.left;
-      highlightCanvas.style.width = styles.width;
-      highlightCanvas.style.height = styles.height;
-      highlightCanvas.width = canvas.width;
-      highlightCanvas.height = canvas.height;
-      highlightCanvas.style.zIndex = '1000';
-
-      const context = canvas.getContext('2d')!;
-      
-      context.fillStyle = 'rgba(255, 0, 0, 0.5)';
-      context.strokeStyle = 'rgba(255, 0, 0, 0.5)';
-      context.lineWidth = 1;
-      context.fillRect(0, 0, canvas.width, canvas.height);
-    }, 100)
   }, []);
 
-  const onRenderTextLayerSuccess = useCallback(() => {
-  }, []);
+  const onRenderSuccess = useCallback(() => {
+    const canvas = document.getElementsByClassName('react-pdf__Page__canvas')[0] as HTMLCanvasElement;
+    console.log(canvas);
+    const styles = window.getComputedStyle(canvas);
+    const highlightCanvas = canvasRef.current!;
+    highlightCanvas.style.position = 'absolute';
+    highlightCanvas.style.top = styles.top;
+    highlightCanvas.style.left = styles.left;
+    highlightCanvas.style.width = styles.width;
+    highlightCanvas.style.height = styles.height;
+    highlightCanvas.width = canvas.width;
+    highlightCanvas.height = canvas.height;
+    highlightCanvas.style.zIndex = '1000';
 
+    const context = canvas.getContext('2d')!;
+    
+    context.fillStyle = 'rgba(255, 0, 0, 0.5)';
+    context.strokeStyle = 'rgba(255, 0, 0, 0.5)';
+    context.lineWidth = 1;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }, []);
 
   return (
     <div id="viewer">
       <Document file={doc.filename} onLoadSuccess={onDocumentLoadSuccess} >
-        <Page pageNumber={page} onRenderTextLayerSuccess={onRenderTextLayerSuccess} >
+        <Page pageNumber={page} onRenderSuccess={onRenderSuccess} >
         </Page>
       </Document>
       <canvas ref={canvasRef} id="highlight-canvas"/>
