ede3e700979942ba3c8f7b38b964cb97343a128a
Raymond Nassar <97557596+raymond-nassar@users.noreply.github.com>
2024-09-18 13:57:26 -0700

    Update Mocks.ts
    Citations from the two financial statements

--- react-pdf-sample/src/Mocks.ts
+++ react-pdf-sample/src/Mocks.ts
@@ -1,65 +1,116 @@
 import { Doc, Citation, ReviewStatus } from './Types'
 
 export const mockDocs: Doc[] = [
-  { filename: './compressed.tracemonkey-pldi-09.pdf', pages: 2 },
-  { filename: './compressed.tracemonkey-pldi-09.pdf', pages: 3 },
+  { filename: './PressReleaseFY24Q3.pdf', pages: 10 },
+  { filename: './Microsoft 10Q FY24Q3 1.pdf', pages: 73 },
 ]
 
 export const mockCitations: Citation[][] = [
-  // "How much would could a wood chuck chuck if a wood chuck could chuck wood?"
+  // "What was the company’s revenue for the third quarter of Fiscal Year 2024?"
   [
     {
-      excerpt: "Woodchucks can chew up to 700 pounds of wood in a single day.",
+      excerpt: "Revenue was $61.9 billion and increased 17%.",
       docIndex: 0,
       reviewStatus: ReviewStatus.Unreviewed,
       page: 1,
       region: { top: 0, left: 0, width: 100, height: 100 },
     },
     {
-      excerpt: "Beavers can chew through wood with their teeth.",
+      excerpt: "61,858",
       docIndex: 1,
+      reviewStatus: ReviewStatus.Unreviewed,
+      page: 29,
+      region: { top: 0, left: 0, width: 100, height: 100 },
+    },
+  ],
+
+  // "What are the earnings per share (EPS) for this quarter?"
+  [
+    {
+      excerpt: "$2.94",
+      docIndex: 0,
+      reviewStatus: ReviewStatus.Unreviewed,
+      page: 1,
+      region: { top: 0, left: 0, width: 100, height: 100 },
+    },
+  ],
+
+  // "How much money did Microsoft return to shareholders in the form of share repurchases?",
+  [
+    {
+      excerpt: "Microsoft returned $8.4 billion to shareholders in the form of share repurchases and dividends in the third quarter of fiscal year 2024.",
+      docIndex: 0,
       reviewStatus: ReviewStatus.Unreviewed,
       page: 2,
       region: { top: 0, left: 0, width: 100, height: 100 },
     },
   ],
 
-  // "What is the airspeed velocity of an unladen swallow?"
+  // "What are the total assets reported?",
   [
     {
-      excerpt: "An African swallow can fly at 11 meters per second.",
-      docIndex: 1,
-      reviewStatus: ReviewStatus.Unreviewed,
-      page: 1,
-      region: { top: 0, left: 0, width: 100, height: 100 },
-    },
-  ],
-
-  // "Why is a raven like a writing desk?",
-  [
-    {
-      excerpt: "Both are used to write things.",
+      excerpt: "484,275",
       docIndex: 0,
       reviewStatus: ReviewStatus.Unreviewed,
-      page: 2,
-      region: { top: 0, left: 0, width: 100, height: 100 },
-    },
-  ],
-
-  // "What is the answer to the ultimate question of life, the universe, and everything?",
-  [
-    {
-      excerpt: "42",
-      docIndex: 0,
-      reviewStatus: ReviewStatus.Unreviewed,
-      page: 1,
+      page: 8,
       region: { top: 0, left: 0, width: 100, height: 100 },
     },
     {
-      excerpt: "Netflix and chill",
+      excerpt: "484,275",
       docIndex: 1,
       reviewStatus: ReviewStatus.Unreviewed,
-      page: 3,
+      page: 5,
+      region: { top: 0, left: 0, width: 100, height: 100 },
+    }
+  ]
+    // "What are the total liabilities reported?",
+  [
+    {
+      excerpt: "231,123",
+      docIndex: 0,
+      reviewStatus: ReviewStatus.Unreviewed,
+      page: 8,
+      region: { top: 0, left: 0, width: 100, height: 100 },
+    },
+    {
+      excerpt: "231,123",
+      docIndex: 1,
+      reviewStatus: ReviewStatus.Unreviewed,
+      page: 5,
+      region: { top: 0, left: 0, width: 100, height: 100 },
+    }
+  ]
+    // "What is the net cash provided by operating activities?",
+  [
+    {
+      excerpt: "31,917",
+      docIndex: 0,
+      reviewStatus: ReviewStatus.Unreviewed,
+      page: 9,
+      region: { top: 0, left: 0, width: 100, height: 100 },
+    },
+    {
+      excerpt: "31,917",
+      docIndex: 1,
+      reviewStatus: ReviewStatus.Unreviewed,
+      page: 6,
+      region: { top: 0, left: 0, width: 100, height: 100 },
+    }
+  ]
+     // "What is the net cash used in financing activities?",
+  [
+    {
+      excerpt: "18,808",
+      docIndex: 0,
+      reviewStatus: ReviewStatus.Unreviewed,
+      page: 9,
+      region: { top: 0, left: 0, width: 100, height: 100 },
+    },
+    {
+      excerpt: "18,808",
+      docIndex: 1,
+      reviewStatus: ReviewStatus.Unreviewed,
+      page: 6,
       region: { top: 0, left: 0, width: 100, height: 100 },
     }
   ]
