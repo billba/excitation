9c3b19e1112a8ed4a43a643e3c557ae4e7cfde1d
Bill Barnes <billba@microsoft.com>
2024-09-20 06:45:36 -0700

    review status

--- react-pdf-sample/src/Sidebar.tsx
+++ react-pdf-sample/src/Sidebar.tsx
@@ -2,14 +2,18 @@ import { useAtom } from 'jotai'
 import { citationsAtom, questionIndexAtom, citationIndexAtom } from "./State";
 import { questions } from "./Questions";
 import { useCallback } from 'react';
+import { ReviewStatus } from './Types';
 
 export function Sidebar() {
   const [questionIndex, setQuestionIndex ] = useAtom(questionIndexAtom);
   const [citationIndex, setCitationIndex] = useAtom(citationIndexAtom);
-  const [citations] = useAtom(citationsAtom);
+  const [citations, setCitations] = useAtom(citationsAtom);
   
   const disablePrev = questionIndex === 0;
   const disableNext = questionIndex === questions.length - 1;
+
+  const questionCitations = citations[questionIndex];
+  const citation = questionCitations[citationIndex];
 
   const prevQuestion = useCallback(() => {
     setQuestionIndex(questionIndex - 1);
@@ -20,6 +24,22 @@ export function Sidebar() {
     setQuestionIndex(questionIndex + 1);
     setCitationIndex(0);
   }, [questionIndex, setQuestionIndex, setCitationIndex]);
+
+  const toggle = useCallback((target: ReviewStatus.Approved | ReviewStatus.Rejected) => () => {
+    console.log('Accepted citation');
+    setCitations([
+      ...citations.slice(0, questionIndex),
+      [
+        ...questionCitations.slice(0, citationIndex),
+        { ...
+          citation,
+          reviewStatus: citation.reviewStatus === target ? ReviewStatus.Unreviewed : target,
+        },
+        ...questionCitations.slice(citationIndex + 1),
+      ],
+      ...citations.slice(questionIndex + 1),
+    ]);
+  }, [citations, setCitations, questionIndex, questionCitations, citationIndex, citation]);
 
   const setCurrentCitation = useCallback((i: number) => () => setCitationIndex(i), [setCitationIndex]);
 
@@ -33,27 +53,31 @@ export function Sidebar() {
       <div className='question'>{questions[questionIndex]}</div>
       <div className='citation-header'>Citations:</div>
       <div>
-        {citations[questionIndex].map(({ excerpt }, i) => (
+        {citations[questionIndex].map(({ excerpt, reviewStatus }, i) => (
           <div
             className={'citation-row' + (i === citationIndex ? ' selected' : '')}
             key={i}
             onClick={setCurrentCitation(i)}
           >
             <div className='citation'>{excerpt}</div>
-            <div className='buttons'>
-              <button
-                className='cite-button'
-                style={{backgroundColor: "palegreen"}}
-              >
-                Y
-              </button>
-              <button
-                className='cite-button'
-                style={{backgroundColor: "pink"}}
-              >
-                N
-              </button>
-            </div>
+              <div className='buttons'>  
+                {i === citationIndex || reviewStatus === ReviewStatus.Approved ?
+                  <button
+                  className='cite-button'
+                  style={{ backgroundColor: reviewStatus === ReviewStatus.Approved ? "palegreen" : 'grey' }}
+                  onClick={toggle(ReviewStatus.Approved)}
+                  disabled={i !== citationIndex}
+                  >✓</button>
+                : null }
+                {i === citationIndex || reviewStatus === ReviewStatus.Rejected ?
+                  <button
+                  className='cite-button'
+                  style={{ backgroundColor: reviewStatus === ReviewStatus.Rejected ? "lightcoral" : 'grey' }}
+                  onClick={toggle(ReviewStatus.Rejected)}
+                  disabled={i !== citationIndex}
+                  >𐄂</button>
+                : null }
+              </div>
           </div>
         ))}
       </div>
