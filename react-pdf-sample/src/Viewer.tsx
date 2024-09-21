fd97fbae2cc8d6154e3c029f401bf26a47826a5f
Bill Barnes <billba@microsoft.com>
2024-09-21 15:37:20 -0700

    format

--- react-pdf-sample/src/Viewer.tsx
+++ react-pdf-sample/src/Viewer.tsx
@@ -1,16 +1,21 @@
-import { useAtom } from 'jotai';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useAtom } from "jotai";
+import { useCallback, useEffect, useRef, useState } from "react";
 
-import { Document, Page } from 'react-pdf';
+import { Document, Page } from "react-pdf";
 
-import { questionIndexAtom, citationIndexAtom, citationsAtom, docsAtom } from './State';
+import {
+  questionIndexAtom,
+  citationIndexAtom,
+  citationsAtom,
+  docsAtom,
+} from "./State";
 
 export function Viewer() {
   const [docs] = useAtom(docsAtom);
   const [questionIndex] = useAtom(questionIndexAtom);
   const [citationIndex] = useAtom(citationIndexAtom);
   const [citations] = useAtom(citationsAtom);
-  
+
   const { docIndex, boundingRegions } = citations[questionIndex][citationIndex];
   let pageNumber: number | undefined = undefined;
   if (boundingRegions && boundingRegions.length > 0) {
@@ -18,40 +23,41 @@ export function Viewer() {
   }
 
   const doc = docs[docIndex];
-  
-  const canvasRef = useRef<HTMLCanvasElement>(null)
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const [renderCounter, setRenderCounter] = useState(0); // this is how we make the highlight responsive to page renders
 
-  const onDocumentLoadSuccess = useCallback(() => {
-  }, []);
+  const onDocumentLoadSuccess = useCallback(() => {}, []);
 
   const onRenderSuccess = useCallback(() => {
-    console.log('onRenderSuccess');
-    const canvas = document.getElementsByClassName('react-pdf__Page__canvas')[0] as HTMLCanvasElement;
+    console.log("onRenderSuccess");
+    const canvas = document.getElementsByClassName(
+      "react-pdf__Page__canvas"
+    )[0] as HTMLCanvasElement;
     const rect = canvas.getBoundingClientRect();
 
     const highlightCanvas = canvasRef.current!;
 
-    highlightCanvas.style.top = rect.top.toString() + 'px';
-    highlightCanvas.style.left = rect.left.toString() + 'px';
-    highlightCanvas.style.width = rect.width.toString() + 'px';
-    highlightCanvas.style.height = rect.height.toString() + 'px';
+    highlightCanvas.style.top = rect.top.toString() + "px";
+    highlightCanvas.style.left = rect.left.toString() + "px";
+    highlightCanvas.style.width = rect.width.toString() + "px";
+    highlightCanvas.style.height = rect.height.toString() + "px";
 
     highlightCanvas.width = canvas.width;
     highlightCanvas.height = canvas.height;
-    setRenderCounter(c => c + 1);
+    setRenderCounter((c) => c + 1);
   }, []);
 
   useEffect(() => {
     const highlightCanvas = canvasRef.current!;
-    const context = highlightCanvas.getContext('2d')!;
-    
+    const context = highlightCanvas.getContext("2d")!;
+
     context.clearRect(0, 0, highlightCanvas.width, highlightCanvas.height);
 
     if (boundingRegions && boundingRegions.length > 0) {
       for (const { polygon } of boundingRegions) {
         console.log(polygon);
-        context.fillStyle = 'yellow';
+        context.fillStyle = "yellow";
         context.beginPath();
         context.moveTo(polygon[0] * 144, polygon[1] * 144);
         context.lineTo(polygon[2] * 144, polygon[3] * 144);
@@ -67,13 +73,24 @@ export function Viewer() {
 
   return (
     <div id="viewer">
-      <div id="viewer-header">{doc.filename} {pageNumber === undefined ? 'CITATION NOT FOUND' : (pageNumber).toString()}</div>
+      <div id="viewer-header">
+        {doc.filename}{" "}
+        {pageNumber === undefined
+          ? "CITATION NOT FOUND"
+          : pageNumber.toString()}
+      </div>
       <div>
-        <Document file={doc.filename} onLoadSuccess={onDocumentLoadSuccess} >
-          { pageNumber && <Page pageNumber={pageNumber} onRenderSuccess={onRenderSuccess} /> }
+        <Document file={doc.filename} onLoadSuccess={onDocumentLoadSuccess}>
+          {pageNumber && (
+            <Page pageNumber={pageNumber} onRenderSuccess={onRenderSuccess} />
+          )}
         </Document>
-        <canvas ref={canvasRef} id="highlight-canvas" style={{ position: 'absolute', zIndex: 1000, opacity: '0.5' }} />
+        <canvas
+          ref={canvasRef}
+          id="highlight-canvas"
+          style={{ position: "absolute", zIndex: 1000, opacity: "0.5" }}
+        />
       </div>
     </div>
-  )
+  );
 }
