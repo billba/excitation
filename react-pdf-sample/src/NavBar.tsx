931fa5b7bdf9b4f63067f394ebff6838f9488ea1
Bill Barnes <billba@microsoft.com>
2024-10-01 09:42:46 -0700

    slightly nicer visual cue for multi-line citations

--- react-pdf-sample/src/NavBar.tsx
+++ react-pdf-sample/src/NavBar.tsx
@@ -14,25 +14,65 @@ export const NavBar = () => {
   );
 
   const pages = docs[docIndex].pages;
-  const pageNumbers = newCitation || ux.citationIndex == undefined ? [] : ux.citationHighlights.map(({ pageNumber }) => pageNumber).sort();
+  const pageNumbers =
+    newCitation || ux.citationIndex == undefined
+      ? []
+      : ux.citationHighlights.map(({ pageNumber }) => pageNumber).sort();
 
   const disablePrev = pageNumber === 1;
   const disableNext = pageNumber === pages - 1;
 
+  const citationPrev = pageNumbers.includes(pageNumber - 1);
+  const citationNext = pageNumbers.includes(pageNumber + 1);
+
   return (
     <div id="navbar">
-      &nbsp;
-      { docs[docIndex].filename }
-      <button onClick={dispatch({ type: "prevPage" })} disabled={disablePrev} className={pageNumbers.includes(pageNumber - 1) ? 'selected' : undefined}>
-        &lt;
-      </button>
-      <span className={pageNumbers.includes(pageNumber) ? 'selected' : undefined}>{pageNumber}</span> / {pages}
-      <button onClick={dispatch({ type: "nextPage" })} disabled={disableNext} className={pageNumbers.includes(pageNumber + 1) ? 'selected' : undefined}>
-        &gt;
-      </button>
-      {!newCitation && ux.citationIndex !== undefined && ux.citationHighlights.length == 0 && (
-        <span className="selected">Unable to locate citation</span>
-      )}
+      <div className="navbar-filename">{docs[docIndex].filename}</div>
+      <div className="navbar-page">
+        <div className="navbar-column">
+          <span
+            className={citationPrev ? "selected clickable" : "hidden"}
+            onClick={citationPrev ? dispatch({ type: "prevPage" }) : undefined}
+          >
+            Citation continues from previous page
+          </span>
+        </div>
+        <div className="navbar-column">
+          <button
+            onClick={dispatch({ type: "prevPage" })}
+            disabled={disablePrev}
+          >
+            &lt;
+          </button>
+          <span
+            className={
+              pageNumbers.includes(pageNumber) ? "selected" : undefined
+            }
+          >
+            {pageNumber}
+          </span>{" "}
+          / {pages}
+          <button
+            onClick={dispatch({ type: "nextPage" })}
+            disabled={disableNext}
+          >
+            &gt;
+          </button>
+        </div>
+        <div className="navbar-column">
+          <span
+            className={citationNext ? "selected clickable" : "hidden"}
+            onClick={citationNext ? dispatch({ type: "nextPage" }) : undefined}
+          >
+            Citation continues on next page
+          </span>
+          {!newCitation &&
+            ux.citationIndex !== undefined &&
+            ux.citationHighlights.length == 0 && (
+              <span className="selected">Unable to locate citation</span>
+            )}
+        </div>
+      </div>
     </div>
   );
 };
