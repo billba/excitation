cb345dd9c77ea65dae63c49d4f7b073c6cdf19ce
Lee Cattarin <lecattar@microsoft.com>
2024-10-04 13:17:11 -0700

    fix spacing :(

--- react-pdf-sample/src/Types.ts
+++ react-pdf-sample/src/Types.ts
@@ -210,25 +210,25 @@ interface Span {
 }
 
 export type Event =
-	| {
-		type: "addCitation";
-		formId: number;
-		questionId: number;
-		documentId: number;
-		excerpt: string;
-		bounds: BoundingRegion[];
-		review: Review;
-		creator: string;
-	}
-	| {
-		type: "updateReview";
-		citationId: number;
-		review: Review;
-		creator: string;
-	}
-	| {
-		type: "updateBounds";
-		citationId: number;
-		bounds: BoundingRegion[];
-		creator: string;
-	};
+  | {
+    type: "addCitation";
+    formId: number;
+    questionId: number;
+    documentId: number;
+    excerpt: string;
+    bounds: BoundingRegion[];
+    review: Review;
+    creator: string;
+  }
+  | {
+    type: "updateReview";
+    citationId: number;
+    review: Review;
+    creator: string;
+  }
+  | {
+    type: "updateBounds";
+    citationId: number;
+    bounds: BoundingRegion[];
+    creator: string;
+  };
