1a946d0d0739cb43e9a2de1dec07e5fc5f2bb5e2
Lee Cattarin <lecattar@microsoft.com>
2024-10-09 13:06:29 -0700

    beginning of bounds->text algo

--- react-pdf-sample/src/State.ts
+++ react-pdf-sample/src/State.ts
@@ -270,8 +270,8 @@ export const stateAtom = atom<State, [Action], void>(
                 const { excerpt, bounds } = findUserSelection(
                   pageNumber,
                   realRange!,
-                  viewer
-                  // doc.response!
+                  viewer,
+                  doc.response!
                 );
                 questions[questionIndex].citations.push({
                   documentId: doc.documentId,
