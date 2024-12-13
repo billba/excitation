e8904d1bd1bd9f5aee455d63c7cb70c90944a0f5
Joshua Phelps <joshua.phelps-c@zurichna.com>
2024-12-13 15:45:39 -0600

    cleanup

--- reference-azure-backend/functions/src/functions/generateBlobSasUrl.ts
+++ reference-azure-backend/functions/src/functions/generateBlobSasUrl.ts
@@ -8,23 +8,21 @@ export async function generateBlobSasUrl(request: HttpRequest, context: Invocati
   try {
     const url = request.query.get("url")
     const blobClient = new BlobClient(url)
-    const blobName = blobClient.name
-    const containerName = blobClient.containerName
-    const accountName = process.env.BLOB_STORAGE_ACCOUNT_NAME
-    const accountKey = process.env.BLOB_STORAGE_ACCOUNT_KEY
-    const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey)
-
+    
     const sasOptions = {
-      containerName: containerName,
-      blobName: blobName,
+      containerName: blobClient.containerName,
+      blobName: blobClient.name,
       permissions: ContainerSASPermissions.parse("r"),
       startsOn: new Date(),
       // TODO: What is a good expires on value?
       expiresOn: new Date(new Date().valueOf() + 3600 * 1000)
     }
-
+    const accountName = process.env.BLOB_STORAGE_ACCOUNT_NAME
+    const accountKey = process.env.BLOB_STORAGE_ACCOUNT_KEY
+    const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey)
     const sasToken = generateBlobSASQueryParameters(sasOptions, sharedKeyCredential).toString()
     const sasUrl = `${blobClient.url}?${sasToken}`
+
     return {
       jsonBody: sasUrl
     };
